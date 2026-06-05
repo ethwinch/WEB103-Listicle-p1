45962cea8eb63f5e5f394134ef883bdef5671868
Ethan-Is-Obsessed <EthanEvo4@gmail.com>
2026-06-05 02:21:20 -0400

    client/scripts/locations.js not displaying data properly

---
+++ GitHub/Web103/P1-Listicle-p1/server/data/locations.js
@@ -0,0 +1,74 @@
+const locationData = [
+    {
+        "id": "0",
+        "name": "The LGBT Center",
+        "borough": "Manhattan",
+        "neighborhood": "Greenwich Village",
+        "type": "Third Space",
+        "masks": "Not Required",
+        "URL": "https://gaycenter.org/",
+        "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.halcyonhealth.us%2Fwp-content%2Fuploads%2F2021%2F04%2Fthecenter-min.jpg&f=1&nofb=1&ipt=defa9d2116714da86071774f0f79b214c2c52e02a68ddbbccb29b1f747e56ba6"
+    },
+    {
+        "id": "1",
+        "name": "BGSQD",
+        "borough": "Manhattan",
+        "neighborhood": "Greenwich Village",
+        "type": "Store",
+        "masks": "Not Required",
+        "URL": "https://www.bgsqd.com/",
+        "image": ""
+    },
+    {
+        "id": "2",
+        "name": "Hivemindbooks",
+        "borough": "Brooklyn",
+        "neighborhood": "Bushwick",
+        "type": "Store",
+        "masks": "Not Required",
+        "URL": "https://www.hivemindbooks.com/",
+        "image": ""
+    },
+    {
+        "id": "3",
+        "name": "The Stonewall Inn",
+        "borough": "Manhattan",
+        "neighborhood": "Greenwich Village",
+        "type": "Store",
+        "masks": "Not Required",
+        "URL": "https://thestonewallinnnyc.com/",
+        "image": ""
+    },
+    {
+        "id": "4",
+        "name": "NYC Resistors",
+        "borough": "Brooklyn",
+        "neighborhood": "Boerum Hill",
+        "type": "Third Space",
+        "masks": "Sometimes Required",
+        "URL": "https://www.nycresistor.com/",
+        "image": ""
+    },
+    {
+        "id": "5",
+        "name": "From Here To Sunday",
+        "borough": "Brooklyn",
+        "neighborhood": "Boerum Hill",
+        "type": "Store",
+        "masks": "Required",
+        "URL": "https://www.heretosunday.com/",
+        "image": ""
+    },
+    {
+        "id": "6",
+        "name": "The Nonbinarian Bookstore",
+        "borough": "Brooklyn",
+        "neighborhood": "Crown Heights",
+        "type": "Store",
+        "masks": "Required",
+        "URL": "https://thenonbinarian.gay/",
+        "image": ""
+    }
+]
+
+export default locationData
